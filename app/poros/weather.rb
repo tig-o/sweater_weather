3429dc0f4780d946117f5d6dcc2929e5fa50cfb8
Thiago Silveira <thiago1992@yahoo.com>
2022-09-26 05:57:17 -0600

    Feat: Weather poros

---
+++ app/poros/weather.rb
@@ -0,0 +1,29 @@
+class Weather
+  attr_reader :id,
+              :datetime,
+              :sunrise,
+              :sunset,
+              :temperature,
+              :feels_like,
+              :humidity,
+              :uvi,
+              :visibility,
+              :conditions,
+              :icon,
+              :hourly_weather,
+              :daily_weather
+
+  def initialize(data)
+    @id = nil
+    @datetime = Time.at(data[:current][:dt])
+    @sunrise = Time.at(data[:current][:sunrise])
+    @sunset = Time.at(data[:current][:sunset])
+    @temperature = data[:current][:temp]
+    @feels_like = data[:current][:feels_like].to_f
+    @humidity = data[:current][:humidity]
+    @uvi = data[:current][:uvi]
+    @visibility = data[:current][:visibility]
+    @conditions = data[:current][:weather][0][:description]
+    @icon = data[:current][:weather][0][:icon]
+  end
+end
