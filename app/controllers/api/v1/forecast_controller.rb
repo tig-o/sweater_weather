94ff6d1fce54848b634828424127457a8b41b34c
Thiago Silveira <thiago1992@yahoo.com>
2022-09-26 06:53:43 -0600

    Feature: Expose and serialize weather forecast on location API endpoint

--- app/controllers/api/v1/forecast_controller.rb
+++ app/controllers/api/v1/forecast_controller.rb
@@ -1,6 +1,8 @@
 class Api::V1::ForecastController < ApplicationController
   def index
-    
+    coordinates = MapFacade.get_latitude_longitude(params[:location])
+    forecast_data = WeatherFacade.get_forecasts(coordinates[:lat], coordinates[:lng])
+    weather_objects = Weather.new(forecast_data)
+    render json: ForecastSerializer.display_forecasts(weather_objects)
   end
-
 end
