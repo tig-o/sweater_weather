ced5e6d77fd09cafc2a24a32940d108be282d2da
Thiago Silveira <thiago1992@yahoo.com>
2022-09-26 11:16:21 -0600

    Refactor: Books controller

--- app/controllers/api/v1/books_controller.rb
+++ app/controllers/api/v1/books_controller.rb
@@ -1,6 +1,12 @@
 class Api::V1::BooksController < ApplicationController
   def index
+    # binding.pry
+    coordinates = MapFacade.get_latitude_longitude(params[:location])
+    forecast_data = WeatherFacade.get_forecasts(coordinates[:lat], coordinates[:lng])
+    weather_objects = Weather.new(forecast_data)
+    book_objects = BookFacade.get_books(params[:location], params[:quantity])
 
+    render json: BookSerializer.display_book_weather_data(weather_objects, book_objects)
   end
-  
+
 end
