9c3f4006517e1f64f24a64de528c3fceb5a4012f
Thiago Silveira <thiago1992@yahoo.com>
2022-09-26 10:09:22 -0600

    Refactor: Book service spec

--- spec/services/book_service_spec.rb
+++ spec/services/book_service_spec.rb
@@ -3,6 +3,11 @@ require 'rails_helper'
 RSpec.describe 'BookService' do
   it 'gets books and info' do
     response = BookService.get_books("denver,co")
-    binding.pry
+    
+    expect(response).to be_a(Hash)
+    expect(response[:numFound]).to be_an(Integer)
+    expect(response[:docs]).to be_an(Array)
+    expect(response[:docs][0][:title]).to be_a(String)
+    expect(response[:docs][0][:isbn]).to be_an(Array)
   end
 end
