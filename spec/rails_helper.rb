b93e71e9494102db2e6439edb2bf27a78803eeeb
Thiago Silveira <thiago1992@yahoo.com>
2022-09-25 23:59:23 -0600

    Add: Simplecov and shoulda matchers setup

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,4 +1,8 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
+require 'simplecov'
+SimpleCov.start
+SimpleCov.add_filter ["app/channels", "app/helpers", "app/mailers", "app/jobs"]
+
 require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
@@ -61,6 +65,13 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
-  
+
   config.include FactoryBot::Syntax::Methods
+end
+
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    with.test_framework :rspec
+    with.library :rails
+  end
 end
