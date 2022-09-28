9c31c5b0184b703c7e8f3c16fa50aeb469f0772e
Thiago Silveira <thiago1992@yahoo.com>
2022-09-28 02:00:03 -0600

    Feat: Return 400 if missing user field(s)

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -4,7 +4,9 @@ class Api::V1::UsersController < ApplicationController
       render json: { message: "email already exists, try another" }, status: 400
     elsif params[:password] != params[:password_confirmation]
       render json: { message: "mismatching passwords, try again" }, status: 400
-    else
+    elsif params[:email] == "" || params[:password] == "" || params[:password_confirmation] == ""
+      render json: { message: "missing field(s), try again" }, status: 400
+    elsif
       user = User.create!(email: params[:email], password: params[:password], password_confirmation: params[:password_confirmation], api_key: SecureRandom.hex(13))
       render json: UserSerializer.display_user(user), status: 201
     end
