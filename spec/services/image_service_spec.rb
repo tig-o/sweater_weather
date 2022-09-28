30bc1218579f8023b139bd251b93f78a37679c0b
Thiago Silveira <thiago1992@yahoo.com>
2022-09-28 08:42:39 -0600

    Test: Refactor image service spec

--- spec/services/image_service_spec.rb
+++ spec/services/image_service_spec.rb
@@ -1,9 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe 'ImageService' do
-  it 'gets image information' do
+  it 'returns image data' do
     response = ImageService.get_image("denver,co")
-    binding.pry
+
     expect(response).to be_a(Hash)
+    expect(response).to have_key(:total)
+    expect(response).to have_key(:results)
+    expect(response[:results]).to be_an(Array)
+    expect(response[:results].first[:id]).to be_a(String)
+    expect(response[:results].first[:urls][:raw]).to be_a(String)
+    expect(response[:results].first[:urls][:full]).to be_a(String)
+    expect(response[:results].first[:urls][:regular]).to be_a(String)
+    expect(response[:results].first[:user][:name]).to be_a(String)
+    expect(response[:results].first[:user][:location]).to be_a(String)
+    expect(response[:results].first[:user][:portfolio_url]).to be_a(String)
   end
 end
