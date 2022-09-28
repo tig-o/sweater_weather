7e0a13f9988caf1a85b9640eecb5db9da7dff06b
Thiago Silveira <thiago1992@yahoo.com>
2022-09-28 01:35:22 -0600

    Feat: Return 400 status if user email exists

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,14 +1,15 @@
 class Api::V1::UsersController < ApplicationController
   def create
-  
-    user = User.create!(email: params[:email], password: params[:password], password_confirmation: params[:password_confirmation], api_key: SecureRandom.hex(13))
-    render json: UserSerializer.display_user(user), status: 201
-    
+    if User.exists?(email: params[:email])
+      render json: { message: "email already exists, try another" }, status: 400
+    else
+      user = User.create!(email: params[:email], password: params[:password], password_confirmation: params[:password_confirmation], api_key: SecureRandom.hex(13))
+      render json: UserSerializer.display_user(user), status: 201
+    end
     # private
     
     # def user_params
     #   params.permit(:email, :password, :password_confirmation)
     # end
-
   end
 end
