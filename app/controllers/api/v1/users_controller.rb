3fa20014257c81f762e13c55b460a7832eb46788
Thiago Silveira <thiago1992@yahoo.com>
2022-09-28 01:46:44 -0600

    Feat: Return 400 if user passwords are mismatching

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -2,6 +2,8 @@ class Api::V1::UsersController < ApplicationController
   def create
     if User.exists?(email: params[:email])
       render json: { message: "email already exists, try another" }, status: 400
+    elsif params[:password] != params[:password_confirmation]
+      render json: { message: "mismatching passwords, try again" }, status: 400
     else
       user = User.create!(email: params[:email], password: params[:password], password_confirmation: params[:password_confirmation], api_key: SecureRandom.hex(13))
       render json: UserSerializer.display_user(user), status: 201
