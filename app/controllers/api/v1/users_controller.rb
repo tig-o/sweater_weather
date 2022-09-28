51834a353133cafcbbf2d381ac3596f689c42650
Thiago Silveira <thiago1992@yahoo.com>
2022-09-28 01:25:24 -0600

    Feature: Users controller exposes and serializes user with happy path

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,5 +1,14 @@
 class Api::V1::UsersController < ApplicationController
   def create
+  
+    user = User.create!(email: params[:email], password: params[:password], password_confirmation: params[:password_confirmation], api_key: SecureRandom.hex(13))
+    render json: UserSerializer.display_user(user), status: 201
     
+    # private
+    
+    # def user_params
+    #   params.permit(:email, :password, :password_confirmation)
+    # end
+
   end
 end
